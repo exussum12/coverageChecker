c2318ead96a94878d6bd4d4b4c01f9e9f133258b
Scott Dutton <scott@exussum.co.uk>
2017-01-11 19:15:58 +0000

    Add autoload

---
+++ autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+spl_autoload_register(function ($className) {
+    $classPrefix = 'exussum12\CoverageChecker';
+    if (strpos($className, $classPrefix) === 0) {
+        $classPrefix = str_replace("\\", "\\\\", $classPrefix);
+        $unprefixed = preg_replace("/^$classPrefix\\\/", '', $className, 1);
+        $filename =
+            __DIR__ . DIRECTORY_SEPARATOR .
+            'src' . DIRECTORY_SEPARATOR .
+            str_replace('\\', DIRECTORY_SEPARATOR, $unprefixed) .
+            '.php';
+
+        if (file_exists($filename)) {
+            require_once $filename;
+        }
+    }
+});
