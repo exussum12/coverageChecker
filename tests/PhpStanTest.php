6d51fdf30ec83e0755472b155ea0d4f95c8fc9fd
Scott Dutton <scott@exussum.co.uk>
2017-07-25 18:54:04 +0100

    Bringing tests back up to 100%

--- tests/PhpStanTest.php
+++ tests/PhpStanTest.php
@@ -28,4 +28,16 @@ class PhpStanTest extends TestCase
 
         $this->assertSame($expected, $this->stan->getLines());
     }
+
+    public function testInvalidLine()
+    {
+        $this->stan->getLines();
+        $this->assertFalse($this->stan->isValidLine("src/PhpStanLoader.php", 45));
+        $this->assertTrue($this->stan->isValidLine("src/PhpStanLoader.php", 41));
+    }
+
+    public function testNotFoundFile()
+    {
+        $this->assertNull($this->stan->handleNotFoundFile());
+    }
 }
