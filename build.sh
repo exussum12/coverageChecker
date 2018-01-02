a8d87e8d5c0569462760dd7a40b0e0917c0bc1d6
Scott Dutton <exussum12@users.noreply.github.com>
2018-01-02 22:35:02 +0000

    Fix coveralls version breaking change

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 git log origin/master... | grep -q SKIP_BUILD && exit 0
 
-[ -z "$UPDATE_COVERAGE" ] || composer require satooshi/php-coveralls:dev-master
+[ -z "$UPDATE_COVERAGE" ] || composer require satooshi/php-coveralls:v1.1.0
 
 composer install --dev
 git diff $(git merge-base origin/master HEAD) > diff.txt
