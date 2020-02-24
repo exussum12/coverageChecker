461472703715ec18c5b1024090dcc8acfea28a62
Scott Dutton <scott@exussum.co.uk>
2020-02-24 07:11:52 +0000

    Remove another warning

--- src/DiffLineHandle/OldVersion/NewFile.php
+++ src/DiffLineHandle/OldVersion/NewFile.php
@@ -8,6 +8,7 @@ class NewFile extends DiffLineHandle
 
     public function handle(string $line)
     {
+        $match = [];
         if (preg_match('#--- a?/(?<filename>.*)#', $line, $match)) {
             $this->diffFileState->setCurrentFile($match['filename']);
         }
