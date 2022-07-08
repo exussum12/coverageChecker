f94270fccd7e81b19394d8e75d0ba359b54fa548
Tomek Wytrębowicz <tomalecpub@gmail.com>
2022-07-08 15:47:44 +0200

    Update `testNoCoveredLines`

--- tests/PhpunitDiffFilterTest.php
+++ tests/PhpunitDiffFilterTest.php
@@ -83,9 +83,16 @@ class PhpunitDiffFilterTest extends TestCase
             __DIR__ . '/fixtures/coverage-change.xml',
         ];
 
-        ob_start();
-        require(__DIR__ . '/../src/Runners/generic.php');
-        $output = ob_get_clean();
-        $this->assertContainsString('No lines found', $output);
+
+        try {
+            ob_start();
+            require(__DIR__ . '/../src/Runners/generic.php');
+        } catch (Exception $e) {
+            $output = ob_get_clean();
+            $this->assertEquals(3, $e->getCode());
+            return;
+        }
+
+        $this->fail("no exception thrown");
     }
 }
